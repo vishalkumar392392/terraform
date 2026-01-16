55d2b75ce71e627fe254b6a1bce522ad155c6347
Vishal <apple@APPLEs-MacBook-Air.local>
2026-01-16 18:14:04 +0530

    working type:loadbalancer

--- terraform_code/sg_eks/sg.tf
+++ terraform_code/sg_eks/sg.tf
@@ -1,21 +1,39 @@
 resource "aws_security_group" "worker_node_sg" {
   name        = "eks-test"
-  description = "Allow ssh inbound traffic"
-  vpc_id      =  var.vpc_id
+  description = "Security group for EKS worker nodes"
+  vpc_id      = var.vpc_id
 
+  # SSH (optional – restrict later)
   ingress {
-    description      = "ssh access to public"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # NodePort range (REQUIRED for LoadBalancer services)
+  ingress {
+    description = "NodePort services"
+    from_port   = 30000
+    to_port     = 32767
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # (Optional but recommended) allow intra-node communication
+  ingress {
+    description = "Worker node to worker node"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    self        = true
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-
 }
